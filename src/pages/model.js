da1917a1a298bd28c2833c09fb719b7e2d9c2929
Nihat <nihatysimsek@gmail.com>
2022-05-29 21:18:43 +0300

    online-3d-viewer added

--- src/pages/model.js
+++ src/pages/model.js
@@ -1,13 +1,18 @@
 import React from "react";
-  
+import * as OV from 'online-3d-viewer';
+
 const Model = () => {
+  OV.Init3DViewerElements(); // init all viewers on the page
   return (
     <div>
       <h1>
         Model Page
       </h1>
+
+      <div className="online_3d_viewer" style={{width:800, height:600}} model="models/room.obj"></div>
+
     </div>
   );
 };
-  
+
 export default Model;
